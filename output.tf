5f2ee348a025fdaf4e734e276e967575012b7c64
gu9 <gss.gurpreetsaini@gmail.com>
2021-09-25 22:29:53 -0400

    updated output file

--- output.tf
+++ output.tf
@@ -8,3 +8,11 @@ output "public_ip_address" {
   value = data.azurerm_public_ip.vm-pub-ip-assigned.*.ip_address
 }
 
+output "jump_box_ip" {
+  value = var.JUMP_BOX
+}
+output "server_fqdn" {
+  description = "The fully qualified domain name (FQDN) of the PostgreSQL server"
+  value       = azurerm_postgresql_server.django-db.fqdn
+}
+
